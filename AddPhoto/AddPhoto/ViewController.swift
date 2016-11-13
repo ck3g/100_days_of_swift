17f8f8a9b210ec49407c6ee173a638cc471e1a6a
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2016-11-13 14:03:10 +0100

    [AddPhoto] Implementation

--- AddPhoto/AddPhoto/ViewController.swift
+++ AddPhoto/AddPhoto/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    // MARK: - View did load
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Outlets
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    // MARK: - Interractions
+
+    @IBAction func tapCameraButton(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
 
+    // MARK: - When user selects Photo
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = selectedImage
+        } else {
+            print("Something went wrong")
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
 
